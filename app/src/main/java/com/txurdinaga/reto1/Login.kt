fd416749a7323ae20817826d0019cf5c15ddb444
sergiooo33 <144447379+sergiooo33@users.noreply.github.com>
2023-10-02 14:12:57 +0200

    Inicio Diseño Home

--- app/src/main/java/com/txurdinaga/reto1/Login.kt
+++ app/src/main/java/com/txurdinaga/reto1/Login.kt
@@ -1,12 +1,35 @@
 package com.txurdinaga.reto1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
+
+        val passwordEditText = findViewById<EditText>(R.id.editTextTextPassword)
+        val loginButton = findViewById<Button>(R.id.btnInicioSesion)
+
+        loginButton.setOnClickListener(View.OnClickListener {
+            val enteredPassword = passwordEditText.text.toString()
+            
+
+
+            if (enteredPassword == correctPassword) {
+                val intent = Intent(this, WelcomeActivity::class.java)
+                intent.putExtra("usuario", correctPassword)
+                startActivity(intent)
+            } else {
+                val incorrectPasswordMessage = getString(R.string.incorrect_password_message)
+                Toast.makeText(this, incorrectPasswordMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
