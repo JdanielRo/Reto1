d45d14cb0fc02f10c51812c3d2176e89d124a3b0
sergiooo33 <144447379+sergiooo33@users.noreply.github.com>
2023-10-02 13:56:16 +0200

    Login DIseño

--- app/src/main/java/com/txurdinaga/reto1/Login.kt
+++ app/src/main/java/com/txurdinaga/reto1/Login.kt
@@ -3,10 +3,10 @@ package com.txurdinaga.reto1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LogInActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.login)
 
     }
 }
