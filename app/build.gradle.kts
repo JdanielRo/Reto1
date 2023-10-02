94e45448b6a181c66a868efa4caade014db9a201
sergiooo33 <144447379+sergiooo33@users.noreply.github.com>
2023-10-02 14:26:26 +0200

    Backend Login
    Hay errores

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.txurdinaga.reto1"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.txurdinaga.reto1"
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
+    implementation("com.google.firebase:firebase-database-ktx:20.2.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/txurdinaga/reto1/Login.kt
+++ app/src/main/java/com/txurdinaga/reto1/Login.kt
@@ -7,29 +7,41 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
+
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
+        val btnInicioSesion = findViewById<Button>(R.id.btnInicioSesion)
+        val editTextCorreo = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val editTextContraseña = findViewById<EditText>(R.id.editTextTextPassword)
 
-        val passwordEditText = findViewById<EditText>(R.id.editTextTextPassword)
-        val loginButton = findViewById<Button>(R.id.btnInicioSesion)
+        btnInicioSesion.setOnClickListener {
+            val correo = editTextCorreo.text.toString()
+            val contraseña = editTextContraseña.text.toString()
 
-        loginButton.setOnClickListener(View.OnClickListener {
-            val enteredPassword = passwordEditText.text.toString()
-            
-
-
-            if (enteredPassword == correctPassword) {
-                val intent = Intent(this, WelcomeActivity::class.java)
-                intent.putExtra("usuario", correctPassword)
-                startActivity(intent)
+            if (correo.isNotEmpty() && contraseña.isNotEmpty()) {
+                auth.signInWithEmailAndPassword(correo, contraseña)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Inicio de sesión exitoso, redirige al usuario a la pantalla Home.
+                            val intent = Intent(this, Home::class.java)
+                            startActivity(intent)
+                        } else {
+                            // Fallo el inicio de sesión, muestra un mensaje de error al usuario.
+                            Toast.makeText(this, "Error al iniciar sesión", Toast.LENGTH_SHORT).show()
+                        }
+                    }
             } else {
-                val incorrectPasswordMessage = getString(R.string.incorrect_password_message)
-                Toast.makeText(this, incorrectPasswordMessage, Toast.LENGTH_SHORT).show()
+                // Asegúrate de que se ingresen tanto el correo como la contraseña.
+                Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show()
             }
-        })
+        }
+
     }
 }
