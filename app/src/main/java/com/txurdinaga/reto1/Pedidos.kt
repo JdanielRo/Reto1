149bfd685fc4a0a9796da696ff93a316a9b4d1dd
sergiooo33 <144447379+sergiooo33@users.noreply.github.com>
2023-10-05 14:11:48 +0200

    Unir Layouts en el Fragment Pedidos

--- app/src/main/java/com/txurdinaga/reto1/Pedidos.kt
+++ app/src/main/java/com/txurdinaga/reto1/Pedidos.kt
@@ -1,25 +1,25 @@
 package com.txurdinaga.reto1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Space
+import android.widget.TextView
+import com.google.firebase.firestore.FirebaseFirestore
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Pedidos.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Pedidos : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,24 +29,43 @@ class Pedidos : Fragment() {
         }
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pedidos, container, false)
+        val view = inflater.inflate(R.layout.fragment_pedidos, container, false)
+        val linearLayout = view.findViewById<LinearLayout>(R.id.linearLayoutScrollPedidos)
+
+        // Repite la inclusión del diseño 5 veces
+        for (i in 1..7) {
+            // Infla el diseño item_layout.xml
+            val itemLayout = inflater.inflate(R.layout.fragment_pedidos_platos, container, false)
+
+            // Agrega el diseño al LinearLayout
+            linearLayout.addView(itemLayout)
+            val space = Space(requireContext())
+            if(i==7){
+                space.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    resources.getDimensionPixelSize(R.dimen.space_height_ultima) // Cambia esto según tu altura deseada
+                )
+            }else{
+                // Agrega un espacio entre cada diseño
+
+                space.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    resources.getDimensionPixelSize(R.dimen.space_height) // Cambia esto según tu altura deseada
+                )
+            }
+
+            linearLayout.addView(space)
+        }
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Pedidos.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             Pedidos().apply {
@@ -57,3 +76,4 @@ class Pedidos : Fragment() {
             }
     }
 }
+
