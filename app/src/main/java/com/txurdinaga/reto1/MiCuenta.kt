54deae8daa0aeefd34c6dad96ad6f1643136fdca
JdanielRo <144447403+JdanielRo@users.noreply.github.com>
2023-10-10 12:22:23 +0200

    logOut funcional

--- app/src/main/java/com/txurdinaga/reto1/MiCuenta.kt
+++ app/src/main/java/com/txurdinaga/reto1/MiCuenta.kt
@@ -1,26 +1,27 @@
 package com.txurdinaga.reto1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/*
-
-A simple [Fragment] subclass.
-Use the [Home.newInstance] factory method to
-create an instance of this fragment.
-*/
 class MiCuenta : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +35,36 @@ class MiCuenta : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mi_cuenta, container, false)
+        val view = inflater.inflate(R.layout.fragment_mi_cuenta, container, false)
+
+        mAuth = Firebase.auth
+        val buttonLogOut = view.findViewById<Button>(R.id.buttonLogOut)
+        buttonLogOut.setOnClickListener{
+            signOutAndStartSignInActivity()
+        }
+        configureGoogleSignIn()
+        return view
+    }
+
+    private fun configureGoogleSignIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        // Crea un cliente de inicio de sesión de Google con las opciones configuradas
+        mGoogleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+    }
+
+    private fun signOutAndStartSignInActivity() {
+        mAuth.signOut()
+
+        mGoogleSignInClient.signOut().addOnCompleteListener(requireActivity()) {
+            val intent = Intent(requireContext(), Login::class.java)
+            startActivity(intent)
+        }
+
+
     }
 
     companion object {
@@ -43,6 +72,8 @@ class MiCuenta : Fragment() {
             MiCuenta().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)}}
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
 }
