4a36797c4ae27280db74f821f83a13e8d1750b2f
JdanielRo <144447403+JdanielRo@users.noreply.github.com>
2023-10-17 13:01:39 +0200

    clases creadas

---
+++ app/src/main/java/com/txurdinaga/reto1/Extra.kt
@@ -0,0 +1,10 @@
+package com.txurdinaga.reto1
+
+class Extra(
+    val idExtra: String,
+    val nombre: String,
+    val precio: String,
+    val stock: Int,
+    val tipo : String
+) {
+}
---
+++ app/src/main/java/com/txurdinaga/reto1/Pedido.kt
@@ -0,0 +1,8 @@
+package com.txurdinaga.reto1
+
+class Pedido(
+    val idPedido: String,
+    val idUsuario: String,
+    val idPlato: List<String>
+) {
+}
