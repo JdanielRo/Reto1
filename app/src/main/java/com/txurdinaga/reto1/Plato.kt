e6f029ab307aefc12373654160ac97fa3bb90564
JdanielRo <144447403+JdanielRo@users.noreply.github.com>
2023-10-17 13:01:11 +0200

    clases creadas

--- app/src/main/java/com/txurdinaga/reto1/Plato.kt
+++ app/src/main/java/com/txurdinaga/reto1/Plato.kt
@@ -1,12 +1,12 @@
 package com.txurdinaga.reto1
 
 data class Plato(
+
+    val idPlato: String,
     val nombre: String,
-    val descripcion: String,
-    val celiaco: Boolean,
-    val calorias: Int,
     val precio: Double,
-    val cantidad: Int,
-    val id_plato :Int/*,
-    val imageUrl: String // Agrega un campo para la URL de la imagen*/
+    val descripcion: String,
+    val stock: Int,
+    val tipo: String
+
 )
