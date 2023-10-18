6de0fb77e3e3fa6f3fa9ae53cb63199a8dcd2efa
sergiooo33 <144447379+sergiooo33@users.noreply.github.com>
2023-10-18 09:09:55 +0200

    corregido

--- app/src/main/java/com/txurdinaga/reto1/Pedidos.kt
+++ app/src/main/java/com/txurdinaga/reto1/Pedidos.kt
@@ -1,7 +1,5 @@
 package com.txurdinaga.reto1
 
-import android.annotation.SuppressLint
-import android.content.ContentValues.TAG
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,18 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
-import com.google.firebase.firestore.FirebaseFirestore
-import android.util.Log // Importa Log para el manejo de errores
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.SeekBar
-import android.widget.Spinner
-import com.google.firebase.firestore.QuerySnapshot
-import com.google.firebase.storage.FirebaseStorage
-import com.bumptech.glide.Glide
-
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -29,14 +15,7 @@ class Pedidos : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    private val db = FirebaseFirestore.getInstance()
-
-    private lateinit var linearLayout: LinearLayout
-
-    private val listaPlatos: MutableList<Plato> = mutableListOf()
-    private val listaMenus: MutableList<Menu> = mutableListOf()
-
-    private val values = listOf(1000, 1500, 2000, 2500)
+    lateinit var linearLayout :LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,21 +25,23 @@ class Pedidos : Fragment() {
         }
     }
 
-    @SuppressLint("MissingInflatedId", "ResourceType")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_pedidos, container, false)
         linearLayout = view.findViewById(R.id.linearLayoutScrollPedidos)
-
         val nombreApartado = view.findViewById<TextView>(R.id.txtNombreApartadoPedidos)
 
-        val itemLayout = view.findViewById<LinearLayout>(R.layout.layout_plato)
+        // Asegúrate de que tu layout tenga un ID adecuado
+        val itemLayout = inflater.inflate(R.layout.layout_plato, container, false) as LinearLayout
         linearLayout.addView(itemLayout)
+
+
 
         return view
     }
+
 
     companion object {
         @JvmStatic
@@ -73,4 +54,3 @@ class Pedidos : Fragment() {
             }
     }
 }
-
