a8c5bc574cc71076c5509977b89938fbaf66ff35
sergiooo33 <144447379+sergiooo33@users.noreply.github.com>
2023-10-18 13:01:09 +0200

    Obtener platos

--- app/src/main/java/com/txurdinaga/reto1/SplashScreen.kt
+++ app/src/main/java/com/txurdinaga/reto1/SplashScreen.kt
@@ -1,20 +1,62 @@
 package com.txurdinaga.reto1
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
 class SplashScreen : AppCompatActivity() {
+
+    private val listaPlatos: ArrayList<Plato> = ArrayList()
+    private val listaExtras: ArrayList<Plato> = ArrayList()
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
 
         Handler().postDelayed({
-
+            obtenerPlatos()
+            obtenerExtras()
             val intent = Intent(this, Login::class.java)
-            startActivity(intent)
+            intent.putExtra("platos", listaPlatos)
 
+            // Aquí, deberías ver el tamaño de la lista de platos en el logcat
+            Log.d("MiApp", "Cantidad de platos obtenidos: ${listaPlatos.size}")
+
+            startActivity(intent)
         }, 2000)
+    }
+
+    private fun obtenerPlatos() {
+        Log.d("MiApp", "Obteniendo Platos")
+        db.collection("Plato")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val idPlato = document.id
+                    val nombre = document.getString("nombre") ?: ""
+                    val precio = document.getDouble("precio") ?: 0.0
+                    val descripcion = document.getString("descripcion") ?: ""
+                    val stock = document.getLong("stock")?.toInt() ?: 0
+                    val tipo = document.getString("tipo") ?: ""
+                    val plato = Plato(idPlato, nombre, precio, descripcion, stock, tipo)
+
+                    // Aquí, deberías ver el nombre de cada plato en el logcat
+                    Log.d("MiApp", "Plato obtenido: ${plato.nombre}")
+
+                    listaPlatos.add(plato)
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.e("MiApp", "Error al obtener platos: ${e.message}", e)
+            }
+    }
+
+    private fun obtenerExtras() {
+        // Aquí puedes implementar la lógica para obtener extras si es necesario.
     }
 }
